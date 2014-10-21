6a5d61e254398794f48c554268354a1dcdf2368a
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-10-21 15:56:37 +0200

    docker is not available on centos/sl 5

--- modules/puppet-docker/manifests/init.pp
+++ modules/puppet-docker/manifests/init.pp
@@ -1,17 +1,28 @@
 class puppet-docker {
 
-  package { 'docker-io':
-    ensure  => latest 
+  case $operatingsystem {
+    centos, Scientific: { $is_redhat = "true" }
+    default: { fail("Unrecognized operating system for puppet-docker module.") }
   }
+  
+  case $lsbmajdistrelease {
+    5: { warning("docker is not available on centos/SL 5!")}
+    6: { 
+  
+        package { 'docker-io':
+          ensure  => latest 
+        }
 
-  file { "/etc/sysconfig/docker":
-    ensure  => "present",
-    content => 'other_args="--exec-driver=lxc"',
-    require => Package['docker-io']
-  }
+        file { "/etc/sysconfig/docker":
+          ensure  => "present",
+          content => 'other_args="--exec-driver=lxc"',
+          require => Package['docker-io']
+        }
 
-  service { 'docker':
-    ensure => running,
-    require => File['/etc/sysconfig/docker']
+        service { 'docker':
+          ensure => running,
+          require => File['/etc/sysconfig/docker']
+        }
+    }
   }
 }
