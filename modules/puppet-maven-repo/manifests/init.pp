2e66da1dcf39e6fddf6f5dc42636b685fbb9d282
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-10-21 15:40:31 +0200

    Update init.pp

--- modules/puppet-maven-repo/manifests/init.pp
+++ modules/puppet-maven-repo/manifests/init.pp
@@ -4,10 +4,15 @@ class puppet-maven-repo {
     centos, Scientific: { $is_redhat = "true" }
     default: { fail("Unrecognized operating system for puppet-storm-repos.") }
   }
+  
+  case $lsbmajdistrelease {
+    5: { $maven_repo = 'apache-maven-epel-5.repo' }
+    6: { $maven_repo = 'apache-maven.repo' }
+  }
 
   file { 'apache-maven.repo':
     path   => '/etc/yum.repos.d/apache-maven.repo',
     ensure => file,
-    source =>  "puppet:///modules/puppet-maven-repo/apache-maven.repo"
+    source =>  "puppet:///modules/puppet-maven-repo/$maven_repo"
   }
 }
