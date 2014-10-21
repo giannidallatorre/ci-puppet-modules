ef7dcd71cfea588667056835369fa5734373f575
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-10-21 15:48:26 +0200

    fix majdistrelease detection

--- modules/puppet-python/manifests/init.pp
+++ modules/puppet-python/manifests/init.pp
@@ -3,16 +3,20 @@ class puppet-python {
   Exec {
     path => ['/bin', '/usr/bin']
   }
-
-  $python_packages = ["python", "python-setuptools"]
-  $easy_install = "easy_install"
-  $pip = "pip"
-
-  if (lsbmajdistrelease == 5) {
-    ## Install python26 on CentOS/SL 5
-    $python_packages = ["python26", "python26-distribute"]
-    $easy_install = "easy_install-2.6"
-    $pip = "pip2.6"
+  
+  case $lsbmajdistrelease {
+   
+    5: {
+        $python_packages = ["python26", "python26-distribute"]
+        $easy_install = "easy_install-2.6"
+        $pip = "pip2.6"
+        }
+   
+    6:  {
+        $python_packages = ["python", "python-setuptools"]
+        $easy_install = "easy_install"
+        $pip = "pip"
+        }
   }
 
   package { $python_packages: ensure => present }
