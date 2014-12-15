c89972e51a8f02e77cd17db0a72efc1b8fe4a38b
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-12-15 16:03:09 +0100

    Removed useless option from the docker config file

--- modules/puppet-docker/manifests/init.pp
+++ modules/puppet-docker/manifests/init.pp
@@ -13,16 +13,6 @@ class puppet-docker {
           ensure  => latest 
         }
 
-        file { "/etc/sysconfig/docker":
-          ensure  => "present",
-          content => 'other_args="--exec-driver=lxc"',
-          require => Package['docker-io']
-        }
-
-        service { 'docker':
-          ensure => running,
-          require => File['/etc/sysconfig/docker']
-        }
     }
   }
 }
